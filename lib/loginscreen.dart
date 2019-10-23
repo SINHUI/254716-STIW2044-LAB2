1fa17d397499c1b910ba05c3b76a4c6d70967458
Sin Hui <hui8628@hotmail.com>
2019-10-23 17:12:59 +0800

    first commit

---
+++ lib/loginscreen.dart
@@ -0,0 +1,150 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: LoginPage(),
+    );
+  }
+}
+
+class LoginPage extends StatefulWidget {
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  final TextEditingController _emcontroller = TextEditingController();
+  String _email = "";
+  final TextEditingController _passcontroller = TextEditingController();
+  String _password = "";
+  bool _isChecked = false;
+
+  @override
+  void initState() {
+    loadpref();
+    print('Init: $_email');
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return WillPopScope(
+        onWillPop: _onBackPressAppBar,
+        child: Scaffold(
+          resizeToAvoidBottomPadding: false,
+          body: new Container(
+            padding: EdgeInsets.all(30.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Image.asset(
+                  'assets/images/splash.png',
+                  scale: 0.8,
+                ),
+                TextField(
+                    controller: _emcontroller,
+                    keyboardType: TextInputType.emailAddress,
+                    decoration: InputDecoration(
+                        labelText: 'Email', icon: Icon(Icons.email))),
+                TextField(
+                  controller: _passcontroller,
+                  decoration: InputDecoration(
+                      labelText: 'Password', icon: Icon(Icons.lock)),
+                  obscureText: true,
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                MaterialButton(
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(20.0)),
+                  minWidth: 300,
+                  height: 50,
+                  child: Text('Login'),
+                  color: Color.fromRGBO(12, 130, 9, 1),
+                  textColor: Colors.white,
+                  elevation: 15,
+                  onPressed: _onLogin,
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                Row(
+                  children: <Widget>[
+                    Checkbox(
+                      value: _isChecked,
+                      onChanged: (bool value) {
+                        _onChange(value);
+                      },
+                    ),
+                    Text('Remember Me', style: TextStyle(fontSize: 16))
+                  ],
+                ),
+                GestureDetector(
+                    onTap: _onRegister,
+                    child: Text('Register New Account',
+                        style: TextStyle(fontSize: 16))),
+                SizedBox(
+                  height: 10,
+                ),
+                GestureDetector(
+                    onTap: _onForgot,
+                    child:
+                        Text('Forgot Account', style: TextStyle(fontSize: 16))),
+              ],
+            ),
+          ),
+        ));
+  }
+
+  void _onLogin() {
+    _email = _emcontroller.text;
+    _password = _passcontroller.text;
+    if (_isEmailValid(_email) && (_password.length > 4)) {
+     print("Login Success");
+    } 
+  }
+
+  void _onRegister() {
+    print('onRegister');
+  }
+
+  void _onForgot() {
+    print('Forgot');
+  }
+
+  void _onChange(bool value) {
+    setState(() {
+      _isChecked = value;
+      savepref(value);
+    });
+  }
+
+  void loadpref() async {
+    print('Inside loadpref()');
+  }
+
+  void savepref(bool value) async {
+    print('Inside savepref');
+    _email = _emcontroller.text;
+    _password = _passcontroller.text;
+  }
+
+  Future<bool> _onBackPressAppBar() async {
+    SystemNavigator.pop();
+    print('Backpress');
+    return Future.value(false);
+  }
+
+  bool _isEmailValid(String email) {
+    return RegExp(r"^[a-zA-Z0-9.]+@[a-zA-Z0-9]+\.[a-zA-Z]+").hasMatch(email);
+  }
+}
+
+class ProgressDialog {
+}
